4147de159d79ed6462f87acab313f79f1df7f86f
Kiramman-dev <kiramman.mirage@gmail.com>
2024-07-30 16:26:52 +0300

    edit later

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <div className="container">
+        <div className="container-header">
+          <p className="app-title">password generator</p>
+
+          <div className="input-group">
+            <input type="text" placeholder="placeholder" />
+            <button>copy password</button>
+          </div>
+        </div>
+
+        <div className="container-content">
+          <div className="checkbox-wrapper">
+            <input type="checkbox" />
+            <label htmlFor="">label</label>
+          </div>
+
+          <div className="checkbox-wrapper">
+            <input type="checkbox" />
+            <label htmlFor="">label</label>
+          </div>
+
+          <div className="checkbox-wrapper">
+            <input type="checkbox" />
+            <label htmlFor="">label</label>
+          </div>
+
+          <div className="checkbox-wrapper">
+            <input type="checkbox" />
+            <label htmlFor="">label</label>
+          </div>
+        </div>
+
+        <div className="container-footer flex-centered">
+          <button className="generate-button">generate</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
