b48356c9b883d7a22cacdbcffb161f8fddee2866
SpaceMirage <mahefa.mirage@gmail.com>
2024-07-30 22:35:19 +0300

    setup front-end app skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,53 @@ function App() {
     <div className="app">
       <div className="container">
         <div className="container-header">
-          <p className="app-title">password generator</p>
+          <span className="container-title">PASSWORD GENERATOR</span>
+        </div>
 
-          <div className="input-group">
-            <input type="text" placeholder="placeholder" />
-            <button>copy password</button>
+        <div className="container-body">
+          <div className="password-display">
+            <input
+              type="text"
+              name="password-display"
+              placeholder="generated-password"
+              readOnly
+            />
+            <button>copy</button>
+          </div>
+
+          <div className="length-wrapper">
+            <div className="tmp">
+              <label htmlFor="">Numbers of characters</label>
+              <span className="char-counter">40</span>
+            </div>
+            <input type="range" min={10} max={25} />
+          </div>
+
+          <div className="checkbox-group">
+            <div className="checkbox-wrapper">
+              <input type="checkbox" />
+              <label htmlFor="">Include uppercase letters</label>
+            </div>
+
+            <div className="checkbox-wrapper">
+              <input type="checkbox" />
+              <label htmlFor="">Include lowercase letters</label>
+            </div>
+
+            <div className="checkbox-wrapper">
+              <input type="checkbox" />
+              <label htmlFor="">Include numbers</label>
+            </div>
+
+            <div className="checkbox-wrapper">
+              <input type="checkbox" />
+              <label htmlFor="">Include symbols</label>
+            </div>
           </div>
         </div>
 
-        <div className="container-content">
-          <div className="checkbox-wrapper">
-            <input type="checkbox" />
-            <label htmlFor="">label</label>
-          </div>
-
-          <div className="checkbox-wrapper">
-            <input type="checkbox" />
-            <label htmlFor="">label</label>
-          </div>
-
-          <div className="checkbox-wrapper">
-            <input type="checkbox" />
-            <label htmlFor="">label</label>
-          </div>
-
-          <div className="checkbox-wrapper">
-            <input type="checkbox" />
-            <label htmlFor="">label</label>
-          </div>
-        </div>
-
-        <div className="container-footer flex-centered">
-          <button className="generate-button">generate</button>
+        <div className="container-footer">
+          <button>generate password</button>
         </div>
       </div>
     </div>
